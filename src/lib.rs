ae3a33db2155ea0951e4591318bd7d06d7015b2e
Anothernaso <antondev@kuthy.com>
2026-02-05 15:11:46 +0100

    Finish implementation and add simple header to README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,44 @@
 #[cfg(feature = "std-sync")]
 pub mod std {
-    use std::sync::{Arc, RwLock};
+    use std::sync::{Arc, RwLock, RwLockReadGuard};
 
+    #[derive(Clone)]
     pub struct ContextGuard<T> {
         ctx: Arc<RwLock<T>>,
+    }
+
+    impl<T> ContextGuard<T> {
+        pub fn new(ctx: T) -> Self {
+            ContextGuard {
+                ctx: Arc::new(RwLock::new(ctx)),
+            }
+        }
+
+        pub fn ctx(&self) -> RwLockReadGuard<'_, T> {
+            self.ctx.read().expect("failed to read ctx")
+        }
     }
 }
 
 #[cfg(feature = "tokio-sync")]
 pub mod tokio {
     use std::sync::Arc;
-    use tokio::sync::RwLock;
+    use tokio::sync::{RwLock, RwLockReadGuard};
 
+    #[derive(Clone)]
     pub struct ContextGuard<T> {
         ctx: Arc<RwLock<T>>,
+    }
+
+    impl<T> ContextGuard<T> {
+        pub fn new(ctx: T) -> Self {
+            ContextGuard {
+                ctx: Arc::new(RwLock::new(ctx)),
+            }
+        }
+
+        pub async fn ctx(&self) -> RwLockReadGuard<'_, T> {
+            self.ctx.read().await
+        }
     }
 }
